345ee25592681af5e1c8fc8028cd3ce74c6488f1
Will <willaftring@gmail.com>
2020-01-17 09:58:17 -0600

    Fixed our TCP client and our Web Server

--- PS-WebClt/PS-Client.ps1
+++ PS-WebClt/PS-Client.ps1
@@ -1,6 +1,11 @@
+param (
+    [string]$Search="localhost",
+    [int]$Port=12345
+)
+
 $Server = "localhost"
 $Port = 12345
-
+$Bytes = [System.Byte[]]::new(2048)
 $TcpClient = New-Object System.Net.Sockets.TcpClient $Server, $Port
 $TcpStream = $TcpClient.GetStream()
 Write-Output "Sending Hello World"
@@ -12,4 +17,7 @@ $Data = ""
 while (($i = $TcpStream.Read($Bytes, 0, $Bytes.Length)) -ne 0) {
     $Data = [System.Text.Encoding]::ASCII.GetString($Bytes, 0, $i)
     Write-Output $Data
+    break
 }
+
+$TcpClient.Close()
--- PS-WebSrv/PS-SocketsCore.ps1
+++ PS-WebSrv/PS-SocketsCore.ps1
@@ -1,5 +1,7 @@
-
-$Bytes = ""
+$Content = Get-Content -Path C:\Users\Will\Development\PS-Site\PS-WebSrv\Sample.html
+$ContentLength = 
+$Response = "HTTP/1.1 200 OK`nContent-Type: text/html`nContent-Length: 1200`n`n$Content"
+$Bytes = [System.Byte[]]::new(2048)
 $TCPListener = [System.Net.Sockets.TcpListener]12345
 $TCPListener.Start()
 while($true)
@@ -15,13 +17,13 @@ while($true)
     while (($i = $TcpStream.Read($Bytes, 0, $Bytes.Length)) -ne 0)
     {
         $Data = [System.Text.Encoding]::ASCII.GetString($Bytes, 0, $i)
-        Write-Output "Received $Data"
+        Write-Output "Received `n$Data"
 
         $Data = $Data.ToUpper()
 
-        $Msg = [System.Text.Encoding]::ASCII.GetBytes("Hello World")
+        $Msg = [System.Text.Encoding]::ASCII.GetBytes($Response)
         $TcpStream.Write($Msg, 0, $Msg.Length)
-        Write-Output "Sent $Data"
+        Write-Output "Sent $Response"
     }
     $TcpClient.Close()
 
